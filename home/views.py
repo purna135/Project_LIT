f5c92e2acc0ca1d2eef456ecf4ebfab8a9f8c9fb
Purna Chandra Mansingh <42216008+purnachandramansingh@users.noreply.github.com>
2018-11-11 00:31:44 +0530

    Update views.py

--- home/views.py
+++ home/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 import os
+from ilio import read, write
 # Create your views here.
 
 
 def index(request):
     if request.method =='POST':
         code = request.POST['code']
-        with open('data/user_code.py', 'w') as f: f.write(code)
+        write('data/user_code.py', code)
         os.system('python data/user_code.py > data/output.txt 2>&1')
 
-        with open('data/output.txt', 'r') as f: content = f.read()
+        content = read('data/output.txt')
         return render(request, 'index.html', {'code': code, 'output':content})
 
     with open('data/source_code.py', 'r') as f: content = f.read()
